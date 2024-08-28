f3c94d532863611f03c8fb803c797e01fa7b2021
mayaraduartez <INTEL002@supernicolini.net>
2024-08-28 12:12:38 -0300

    profile quase pronto

--- controllers/principalController.js
+++ controllers/principalController.js
@@ -6,13 +6,51 @@ async function principal(req,res){
     res.render("principal/principal.ejs");
 }
 
+async function abrirprefil(req,res){
+    res.render("users/perfil.ejs");
+}
+
 async function favoritos(req,res){
     res.render("users/favoritos.ejs");
 }
 
+async function meuspedidos(req,res){
+    res.render("users/pedidos.ejs");
+}
 
+async function principaladm(req,res){
+    res.render("admin/principal.ejs");
+}
+
+async function createcardapio(req,res){
+    res.render("admin/form-create.ejs");
+}
+
+async function pedidos(req,res){
+    res.render("admin/pedidos.ejs");
+}
+
+async function perfiladm(req,res){
+    res.render("admin/perfil.ejs");
+}
+
+async function login(req,res){
+    res.render("login/login.ejs");
+}
+
+async function cadastro(req,res){
+    res.render("login/cadastro.ejs");
+}
 
 module.exports = {
     principal,
     favoritos,
+    abrirprefil,
+    meuspedidos,
+    principaladm,
+    createcardapio,
+    pedidos,
+    perfiladm,
+    login,
+    cadastro
 }
--- router/principalRoute.js
+++ router/principalRoute.js
@@ -2,9 +2,29 @@ const express = require("express");
 const router = express.Router();
 const principalController = require("../controllers/principalController"); 
 
+//principal
 router.get('/', principalController.principal);
 
+//users
+router.get('/perfil', principalController.abrirprefil);
+
 router.get('/favoritos', principalController.favoritos); 
+
+router.get('/meuspedidos', principalController.meuspedidos);
+
+//adm
+router.get('/principaladm', principalController.principaladm);
+
+router.get('/criarcardapio', principalController.createcardapio);
+
+router.get('/pedidos', principalController.pedidos);
+
+router.get('/perfiladm', principalController.perfiladm);
+
+router.get('/login', principalController.login);
+
+router.get('/cadastro', principalController.cadastro);
+
 
 
 module.exports = router;
